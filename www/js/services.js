bcda1a1be19a9208fe0a38026849344e7f8d56d0
Janós Potecki <janospotecki@yahoo.de>
2016-04-21 11:06:36 +0200

    more cleaning

--- www/js/services.js
+++ www/js/services.js
@@ -106,8 +106,6 @@ angular.module('app.services', [])
                 var time = 0;
                 sessions = data.dailyUseSessions;
                 for(var i = 0; i < sessions.length; i++){
-                    console.log(sessions[i].end);
-                    console.log(sessions[i].begin);
                     if(sessions[i].end == 0)
                         continue;
                     var begin = new Date (Date.parse(sessions[i].begin)).getTime();
